0d1a68b6294443ece20ed0fc9e452662dc69ca82
wesleymktd <wesleymktd@gmail.com>
2022-09-20 19:10:47 -0300

    correção 1 e 2

--- src/average.js
+++ src/average.js
@@ -13,18 +13,18 @@
 
 const average = (array) => {
   let sum = 0;
-  if(array.length === 0) {
-    return undefined
+  if (array.length === 0) {
+    return undefined;
   }
-  for(let index = 0; index < array.length; index += 1) {
-    if(typeof(array[index]) !== 'number') {
-      return undefined  
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number') {
+      return undefined;  
     } 
     
     sum += array[index];
   }
   
-  let media = Math.round((sum / array.length))
+  let media = Math.round((sum / array.length));
   return media;
 };
 
