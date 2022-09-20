1c017d6ba6b60164c772d71af4f725dfec6bf1ef
wesleymktd <wesleymktd@gmail.com>
2022-09-20 17:35:11 -0300

    requisito 1 ok

--- src/average.js
+++ src/average.js
@@ -11,6 +11,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {} ;
+const average = (array) => {
+  let sum = 0;
+  if(array.length === 0) {
+    return undefined
+  }
+  for(let index = 0; index < array.length; index += 1) {
+    if(typeof(array[index]) !== 'number') {
+      return undefined  
+    } 
+    
+    sum += array[index];
+  }
+  
+  let media = Math.round((sum / array.length))
+  return media;
+};
 
 module.exports = average;
